9b16304e62cb124a0d5b57fe41be60beea806c96
Novadis <dark.novadis@gmail.com>
2024-06-21 14:35:07 +0300

    first task commit

---
+++ tests/Valid.test.js
@@ -0,0 +1,55 @@
+const { expect } = require("chai");
+const { clickElement, putText, getText } = require("../lib/commands.js");
+
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("http://qamid.tmweb.ru/client/index.php");
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("qamid.tmweb.ru tests", () => {
+  // beforeEach(async () => {
+  //   const filmTimetLink = await page.$("li:nth-child(2) > a");
+  //   const reservSelector = ".acceptin-button";
+  // });
+
+  test("One place check", async () => {
+    const filmTimetLink = await page.$("li:nth-child(2) > a");
+    const reservSelector = ".acceptin-button";
+    await filmTimetLink.click();
+    await clickElement(page, "div:nth-child(8) > span:nth-child(4)");
+    await page.waitForSelector(reservSelector);
+    await page.click(reservSelector, {timeout: 3000});
+    await page.waitForSelector(reservSelector, {visible: true,});
+    const actual = page.$eval(reservSelector, (link) => link.textContent);
+    expect(actual).toContain("Получить код бронирования");
+  });
+  
+  test("Three place check", async () => {
+    const filmTimetLink = await page.$("li:nth-child(2) > a");
+    const reservSelector = ".acceptin-button";
+    await filmTimetLink.click();
+    await clickElement(page, "div:nth-child(8) > span:nth-child(4)");
+    await clickElement(page, "div:nth-child(6) > span:nth-child(5)");
+    await clickElement(page, "div:nth-child(2) > span:nth-child(2)");
+    await page.waitForSelector(reservSelector);
+    await page.click(reservSelector, {timeout: 3000});
+    await page.waitForSelector(reservSelector, {visible: true,});
+    const actual = page.$eval(reservSelector, (link) => link.textContent);
+    expect(actual).toContain("Получить код бронирования");
+  });
+
+  test("Disabled place check", async () => {
+    const filmTimetLink = await page.$("li:nth-child(2) > a");
+    const reservSelector = ".acceptin-button";    
+    await filmTimetLink.click();
+    await clickElement(page, "div:nth-child(9) > span.buying-scheme__chair.buying-scheme__chair_disabled");
+    const actual = page.$eval(reservSelector, {visible: false,})
+    expect (actual);
+  });
+});
