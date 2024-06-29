78688896256429137ed94280b6b4d437980c2e28
Novadis <dark.novadis@gmail.com>
2024-06-29 19:20:40 +0300

    second task commit

--- features/search.feature
+++ features/search.feature
@@ -1,6 +1,6 @@
 Feature: Search a course
     Scenario: Testing place check
         Given user is on "/client/index.php" page
-        When user check place "div:nth-child(8) > span:nth-child(4)"
+        When user check film "li:nth-child(2) > a"
         Then user see page with code "Получить код бронирования"
 
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -5,12 +5,10 @@ const { Given, When, Then, Before, After } = require("cucumber");
 const { putText, getText } = require("../../lib/commands.js");
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const browser = await puppeteer.launch({ headless: false, slowMo: 30 });
   const page = await browser.newPage();
   this.browser = browser;
   this.page = page;
-  const filmTimetLink = await page.$("li:nth-child(2) > a");
-  const reservSelector = ".acceptin-button";
 });
 
 After(async function () {
@@ -25,16 +23,17 @@ Given("user is on {string} page", async function (string) {
   });
 });
 
-When("user check place {string}", async function (string) {
-    await page.waitForSelector(filmTimetLink, {timeout: 3000});
-    await filmTimetLink.click();
-    await clickElement(page, string);
-    await page.waitForSelector(reservSelector);
-    await page.click(reservSelector, { timeout: 3000 });
+When("user check film {string}", async function (string) {
+  await this.page.waitForSelector(string, { timeout: 3000 });
+  await this.page.click(string);
+  await this.page.waitForSelector("div:nth-child(8) > span:nth-child(4)");
+  await this.page.click("div:nth-child(8) > span:nth-child(4)");
+  await this.page.waitForSelector(".acceptin-button");
+  await this.page.click(".acceptin-button", { timeout: 3000 });
 });
 
 Then("user see page with code {string}", async function (string) {
-  await page.waitForSelector(reservSelector, { visible: true });
-  const actual = await page.$eval(reservSelector, (link) => link.textContent);
+  await this.page.waitForSelector(".acceptin-button", { visible: true });
+  const actual = await this.page.$eval(".acceptin-button",(link) => link.textContent);
   expect(actual).contain(string);
 });
